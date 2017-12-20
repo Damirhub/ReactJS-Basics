596fde63e2a6c54c968f0aa7afed84e59970fc08
Damir-bucket <damir.pjetlovic@gmail.com>
2017-12-20 17:29:58 +0100

    reset, timer

--- 7-8/src/components/Home.js
+++ 7-8/src/components/Home.js
@@ -7,29 +7,67 @@ export class Home extends React.Component {
   constructor(props) {
     super();
       this.state = {
-        age: props.age
+        age: props.age,
+        status: 0
       }
+      setInterval(() => {
+        this.setState({
+          status: this.state.status + 0.01
+        })
+        
+      }, 10);
   }
+ 
 
   makeOlder() {
     this.setState ({
       age: this.state.age + 3
     });
+    if( this.state.age > 50 ){
+      this.reset ();
+    }
   }
+
 
   makeYounger() {
     this.setState ({
       age: this.state.age - 3
+  
     });
+
+    if( this.state.age < 1 ){
+      this.reset ();
+    }
   }
 
   reset () {
     this.setState ({
       age: this.props.age
+     
     })
   }
 
+  resetTimer () {
+    this.setState ({
+      status : 0
+    })
+  }
+
+ 
+  
+
   render() {
+    var min = 1;
+    
+    let stat = Math.round(this.state.status*100)/100;  
+    
+      if(stat > 60 ){
+        this.resetTimer();
+        min ++;
+     }
+
+
+   
       return (
         <div>
 
@@ -45,7 +83,14 @@ export class Home extends React.Component {
 
             <button onClick = { () => this.reset() } className = "btn btn-danger">Reset</button>
 
+            <h3>Status: {min} {stat}</h3>
+
+
+            <button onClick = { () => this.resetTimer() } className = "btn btn-warning">timer reset </button>
+
         </div>
     )
   }
 }
+
+
