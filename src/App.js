09f89daa447c64f2625a30bf1a67eb344a5d0370
Damir-bucket <damir.pjetlovic@gmail.com>
2017-12-20 11:20:00 +0100

    # 6

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
-
 import { Header } from "./components/Header";
 import { Home } from "./components/Home";
-
-
 
 
 class App extends React.Component {
 
   render() {
 
-    var user = {
-      name: "Anna",
-      hobbies: ["sports ", "mama ", 99]
-    };
-
-
-       
+let user = {
+  name:"Amma", 
+  hobbies: ["sports", "jumping", "fucking"]
+}
+     
     return (
       <div className = "container ">
         <div className = "row">
@@ -28,9 +23,10 @@ class App extends React.Component {
 
         <div className = "row">
           <div className="col-xs-10 col-xs-offset-1">
-              <Home name = {"KAX"} age = {"632"}  user = {user} />
-              
-              
+              <Home  name = {"PERA"} age = {39} user = {user} >
+              <p>This is a paragra</p>
+              </Home>
+                      
         </div>
         </div>
       </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,32 +5,38 @@ export class Home extends React.Component {
   render(){
     var text = "Something";
 
-    let mobi = this.props.user.hobbies.map(hobi => {
-      return <li>{hobi}</li>
+  let hob =   this.props.user.hobbies.map ( (bibi, i) =>{ 
+    return <li key = {i}>{bibi}</li>
     })
-     
+  
 
     return(     
-        <div>
+      <div>
           <p>In a new Component! </p>
           <h4>Da name IS {this.props.name}</h4>
           <p>{text}</p>
-          <h4>User Object => Name: {this.props.user.name} </h4>     
+        <h4>User Object => Name: {this.props.user.name} </h4>     
  
-      <div>
-        <h3>Hobbies</h3>
-        {this.props.user.hobbies[1]}
-        {mobi}
+         <div>
+            <h3>Hobbies</h3>
+             {this.props.user.name}
         <hr/>
 
-        {
-          this.props.user.hobbies.map( (bibi, i) =>
-          <li key = {i}>{bibi}</li>)
-        }
-     
-         
+         {
+               this.props.user.hobbies.map( (hobi, i) =>
+              <li key = {i}>{hobi}</li>)
+
+          }
+         <h2>------------------------</h2>
+
+         {this.props.children}
+         {this.props.children}
+         {this.props.children}
+
         </div>
     </div>
     )
   }
 }
+
+
